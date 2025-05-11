bfbe062f314686cb014c9d87e038db9638fd012d
Korolenko <korolenkoalexey84@gmail.com>
2025-05-11 14:48:39 +0300

    third commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,12 +1,42 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const btn = document.querySelector('.button');
-const input = document.querySelector('.input');
-const fulfill = document.querySelector('.fulfilled-radio');
-const reject = document.querySelector('.rejected-radio');
 
+const form = document.querySelector('.form');
 
-btn.addEventListener('click', () => {
-    
-})
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const delay = Number(e.target.delay.value.trim());
+    const state = form.elements.state.value;
+
+    createPromise(delay, state)
+        .then((delay) => {
+            iziToast.success({
+                title: "✅ Fulfilled",
+                message: `Promise in ${delay}ms`,
+                position: "topRight",
+                timeout: 3000,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: "❌ Rejected",
+                message: `Promise in ${delay}ms`,
+                position: "topRight",
+                timeout: 3000,
+            });
+        });
+    form.reset();
+});
+
+function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+}
